ccca77dd6fa8fe97c102dcdc15b03ae3b6862bda
jmartenstein <jmartenstein@gmail.com>
2013-08-29 00:56:32 -0700

    Add index.html file

--- routing.js
+++ routing.js
@@ -1,15 +1,22 @@
+var fs = require('fs');
 var flatiron = require('flatiron'),
   app = flatiron.app;
+
 
 app.use(flatiron.plugins.http, {
   // HTTP options
 });
 
-app.router.get('/version', function () {
+var text = fs.readFileSync('index.html');
+var buffer = new Buffer(text);
 
+app.router.get('/', function () {
+  this.res.end(buffer.toString());
+});
+
+app.router.get('/version', function () {
   this.res.writeHead(200, { 'Content-Type': 'text/plain' });
   this.res.end( 'flatiron' + flatiron.version );
-
 });
 
 app.start(8080);

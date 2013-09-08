a0a154eae8d0fa624748b2f9f24b276bec0931a4
jmartenstein <jmartenstein@gmail.com>
2013-09-08 00:25:46 -0700

    Move static files under 'static'

--- routing.js
+++ routing.js
@@ -1,25 +1,25 @@
-var fs = require('fs');
+// routing.js
+
 var flatiron = require('flatiron'),
   app = flatiron.app;
 
-
-app.use(flatiron.plugins.http, {
-  // HTTP options
+app.use(flatiron.plugins.http);
+app.use(flatiron.plugins.static, { 
+  dir: __dirname + '/static',
+  index: 'index.html'
 });
 
-var text_index = fs.readFileSync('index.html');
-var buff_index = new Buffer(text_index);
-
-app.router.get('/', function () {
-  this.res.end(buff_index.toString());
+app.router.get('/headers', function () {
+  this.res.json(this.req.headers);
 });
 
 app.router.get('/version', function () {
   this.res.writeHead(200, { 'Content-Type': 'text/plain' });
-  this.res.end( 'flatiron' + flatiron.version );
+  this.res.end( 'flatiron ' + flatiron.version );
 });
 
 var port = process.env.PORT || 5000;
 app.start(port, function() {
   console.log("Listening on " + port);
+  console.log("Directory " + __dirname + '/static');
 });
